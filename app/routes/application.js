ed82dcffd7197937e604145807000123b11d1bc0
Nic Nash <nicnash08@gmail.com>
2015-10-01 14:03:52 -0500

    robert changes

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,22 +5,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	// authenticator: 'authenticator:torii',
 	
-	ajaxCall: function(url, im){
-		// $.ajaxSetup({
-		//     headers: { 'Authorization': 'Basic dXNlckBleGFtcGxlLmNvbTpwYXNzd29yZA' }
-		// });
-		Ember.$.ajaxPrefilter(function(Authorization, oriOpt, jqXHR) {
-		    jqXHR.setRequestHeader("Authorization", "Basic dXNlckBleGFtcGxlLmNvbTpwYXNzd29yZA");
-		});
+	ajaxCall: function(url, username,password){
+		
 	      return Ember.$.ajax({
 	        url        : url,
 	        contentType: "application/json; charset=utf-8",
-	        data       : JSON.stringify(im),
-	        type       : "POST",
+	        type       : "GET",
 	        dataType   : "json",
-	        
-	        // traditional: true
-	        // xhrFields  : {withCredentials: true}
+	        beforeSend(xhr){
+	        	xhr.setRequestHeader('Authorization', 'Basic ' + window.btoa(`${username}:${password}`));
+	        }
 
 	      });},
 
@@ -33,18 +27,16 @@ export default Ember.Route.extend({
 			console.log('password',password);
 
 
-			var im = {
-					     Authorization:'empty'
-					 };
+			
 
-			self.ajaxCall("http://127.0.0.1:3000/token",im)
+			self.ajaxCall("http://127.0.0.1:3000/token",username,password)
 				.then(function(result) {
 					console.log('result',result);
 				},function(err){
 					console.log('error');
 					console.log(err);
 				});
-				;
+				
 
 				console.log('asdfasf');
 
@@ -56,29 +48,3 @@ export default Ember.Route.extend({
         }
 	}
 });
-
-
-
-// ajaxCall                    : function(url, inputModel) {
-//       var im = inputModel || this.options.inputModel;
-//       return Ember.$.ajax({
-//         url        : this.options.APIRoot + url,
-//         contentType: "application/json; charset=utf-8",
-//         data       : JSON.stringify(im),
-//         type       : "POST",
-//         dataType   : "json",
-//         traditional: true,
-//         xhrFields  : {withCredentials: true}
-
-//       });
-
-// options: {
-//       APIRoot   : config.apiURL,
-//       inputModel: {apiKey: "APIKey-WEB"}
-//     },
-
-//     //user/authenticate
-//     authenticateUser            : function(userName, password) {
-//       var im = Ember.$.extend({}, this.options.inputModel, {UserName: userName, Password: password});
-//       return this.ajaxCall("user/authenticate", im);
-//     },
